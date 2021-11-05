ed00ff005ce86bb4a8a14a9e1c7754fbe231f51e
Jerry <jerryappleid761208@gmail.com>
2021-11-05 16:01:25 -0700

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default function App(): JSX.Element {
   return (
-    <div>
-      <React.StrictMode>Hello World</React.StrictMode>
-    </div>
+    <React.StrictMode>
+      <div className="flex justify-center items-center bg-gray-100 h-screen">
+        <p className="text-lg text-center font-semibold">
+          Hello World Tailwindcss!
+        </p>
+      </div>
+    </React.StrictMode>
   );
 }
